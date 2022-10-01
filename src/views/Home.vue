38eaad935410c57ec5a283d80c7bee1659727e2b
青游 <2517789608@qq.com>
2022-10-01 22:29:44 +0800

    Home 页面的 css

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <Topnav />
+    <Topnav/>
     <div class="banner">
       <h1>咕咕 UI</h1>
       <h2>一个简约便利的 UI 框架</h2>
@@ -13,12 +13,33 @@
 </template>
 
 <script lang="ts">
-import Topnav from '../components/Topnav.vue'
+import Topnav from '../components/Topnav.vue';
+
 export default {
   components: {Topnav}
-}
+};
 </script>
 
 <style lang="scss" scoped>
-
+.banner {
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: lightgreen;
+  > .actions{
+    padding: 8px 0;
+    a {
+      margin:0 8px;
+      background: #fff;
+      display: inline-block;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 8px;
+    }
+  }
+}
 </style>
