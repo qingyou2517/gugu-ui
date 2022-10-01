5d53f015038bbcc9fca0a7ab5bc0eb733f5a8184
青游 <2517789608@qq.com>
2022-10-02 00:07:55 +0800

    让Topnav响应式，并完善切换aside

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,10 @@ import {provide, ref} from 'vue'
 
 export default {
   name: 'App',
-  setup(){
-    const menuVisible = ref(false)
-    provide('menuVisible', menuVisible) // set
-  }
+  setup() {
+    const width = document.documentElement.clientWidth;
+    const menuVisible = ref(width <= 500 ? false : true);
+    provide("menuVisible", menuVisible); // set
+  },
 }
 </script>
