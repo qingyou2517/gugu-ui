6466f2ae5050b537c7c899f0bb24e3de56a54e9a
青游 <2517789608@qq.com>
2022-10-01 22:06:58 +0800

    搭建html框架

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './index.css'
+import './index.scss'
 import {createRouter, createWebHashHistory} from 'vue-router';
-import Frank from './components/Frank.vue'
-import Frank2 from './components/Frank2.vue'
+import Home from './views/Home.vue'
+import Doc from  './views/Doc.vue'
 
 const history=createWebHashHistory()
 const router=createRouter({
@@ -11,11 +11,11 @@ const router=createRouter({
   routes:[
     {
       path:'/',
-      component:Frank
+      component:Home
     },
     {
-      path:'/xxx',
-      component:Frank2
+      path:'/doc',
+      component:Doc
     },
   ]
 })
