fc6160a55d60d75f581208710954dd2045d20e10
青游 <2517789608@qq.com>
2022-10-02 00:52:54 +0800

    路由切换之后关闭aside

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
 
 <script>
 import {provide, ref} from 'vue'
+import {router} from './router'
 
 export default {
   name: 'App',
@@ -11,6 +12,11 @@ export default {
     const width = document.documentElement.clientWidth;
     const menuVisible = ref(width <= 500 ? false : true);
     provide("menuVisible", menuVisible); // set
+    router.afterEach(() => {
+      if (width <= 500) {
+        menuVisible.value = false;
+      }
+    });
   },
 }
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './index.scss'
-import {createRouter, createWebHashHistory} from 'vue-router';
-import Home from './views/Home.vue'
-import Doc from  './views/Doc.vue'
+import {createApp} from 'vue';
+import App from './App.vue';
+import './index.scss';
+import {router} from './router';
 
-const history=createWebHashHistory()
-const router=createRouter({
-  history,
-  routes:[
-    {
-      path:'/',
-      component:Home
-    },
-    {
-      path:'/doc',
-      component:Doc
-    },
-  ]
-})
-
-const app=createApp(App)
-app.mount(('#app'))
-app.use(router)
+const app = createApp(App);
+app.mount(('#app'));
+app.use(router);
 
