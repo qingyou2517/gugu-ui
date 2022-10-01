424be45f5047466140a0bff15dfe1556482f6e78
青游 <2517789608@qq.com>
2022-10-01 23:49:19 +0800

    切换 aside

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,17 +1,27 @@
 <template>
   <div class="topnav">
-    <div class="logo">LOGO</div>
+    <div class="logo" @click="toggleMenu">LOGO</div>
     <ul class="menu">
       <li>菜单1</li>
       <li>菜单2</li>
     </ul>
   </div>
 </template>
-<script lang="ts">
-export default {
 
-}
+<script lang="ts">
+import {inject, Ref} from 'vue';
+
+export default {
+  setup() {
+    const menuVisible = inject<Ref<boolean>>('menuVisible'); // get
+    const toggleMenu = () => {
+      menuVisible.value = !menuVisible.value;
+    };
+    return {toggleMenu};
+  }
+};
 </script>
+
 <style lang="scss" scoped>
 .topnav {
   background: pink;
@@ -19,14 +29,17 @@ export default {
   padding: 16px;
   position: relative;
   z-index: 10;
+
   > .logo {
     max-width: 6em;
     margin-right: auto;
   }
+
   > .menu {
     display: flex;
     white-space: nowrap;
     flex-wrap: nowrap;
+
     > li {
       margin: 0 1em;
     }
