c0271732f0b325620d09b97c917073355a090915
青游 <2517789608@qq.com>
2022-10-01 22:33:22 +0800

    初始化 aside 的 html

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,8 +5,8 @@
       <h1>咕咕 UI</h1>
       <h2>一个简约便利的 UI 框架</h2>
       <p class="actions">
-        <a>GitHub</a>
-        <a>开始</a>
+        <a href="https://github.com">GitHub</a>
+        <router-link to="/doc">开始</router-link>
       </p>
     </div>
   </div>
