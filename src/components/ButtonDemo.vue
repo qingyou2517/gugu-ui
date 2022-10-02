3cc18f1a2fa71dd4d5639b4d0b499ec583e101c2
青游 <2517789608@qq.com>
2022-10-02 15:46:21 +0800

    创建Button组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,5 +1,23 @@
 <template>
+  <div>Button 示例</div>
+  <h1>示例1</h1>
   <div>
-    Button 示例
+    <Button @click="onClick"
+            @focus="onClick"
+            @mouseover="onClick"
+    >你好</Button>
   </div>
 </template>
+<script lang="ts">
+import Button from '../lib/Button.vue';
+
+export default {
+  components: {Button},
+  setup(){
+    const onClick = ()=>{
+      console.log('hi')
+    }
+    return {onClick}
+  }
+};
+</script>
