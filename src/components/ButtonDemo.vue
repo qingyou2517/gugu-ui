de43bc39a7c9aa7ab7b9f2d8880321b1f629fcbc
青游 <2517789608@qq.com>
2022-10-02 16:28:27 +0800

    Button 基础样式

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -8,16 +8,14 @@
     <Button theme="text">你好</Button>
   </div>
 </template>
+
 <script lang="ts">
 import Button from '../lib/Button.vue';
 
 export default {
   components: {Button},
   setup(){
-    const onClick = ()=>{
-      console.log('hi')
-    }
-    return {onClick}
+
   }
 };
 </script>
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,10 +1,11 @@
 <template>
-  <button class="gugu-button"
-          :class="`theme-${theme}`">
+  <button class="gugu-button" :class="`theme-${theme}`">
     <slot />
   </button>
 </template>
 <script lang="ts">
+import {computed} from 'vue';
+
 export default {
   inheritAttrs: false,
   props: {
@@ -12,15 +13,59 @@ export default {
       type: String,
       default: 'button'
     },
+    size: {
+      type: String,
+      default: "normal",
+    },
   },
-  setup(props, context) {
-    const { size, ...rest } = context.attrs;
-    return { size, rest };
+  setup(props) {
+    const { theme, size } = props;
+    const classes = computed(() => {
+      return {
+        [`gugu-theme-${theme}`]: theme,
+        [`gugu-size-${size}`]: size,
+      };
+    })
+    return { classes };
   },
 };
 </script>
+
 <style lang="scss" scoped>
-div {
-  border: 1px solid red;
+$h: 32px;
+$border-color: #d9d9d9;
+$color: #333;
+$blue: #40a9ff;
+$radius: 4px;
+.gugu-button {
+  box-sizing: border-box;
+  height: $h;
+  padding: 0 12px;
+  cursor: pointer;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  white-space: nowrap;
+  background: white;
+  color: $color;
+  border: 1px solid $border-color;
+  border-radius: $radius;
+  box-shadow: 0 1px 0 fade-out(black, 0.95);
+  //第一个 .gugu-button 不执行 margin-left: 8px;
+  & + & {
+    margin-left: 8px;
+    margin-top: 4px;
+  }
+  &:hover, &:focus {
+    color: $blue;
+    border-color: $blue;
+  }
+  &:focus {
+    outline: none;
+  }
+  //:focus 兼容 firefox 的写法
+  &::-moz-focus-inner {
+    border: 0;
+  }
 }
 </style>
