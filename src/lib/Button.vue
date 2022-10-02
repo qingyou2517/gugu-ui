fcadf30b5b69ae6f0feb40cd4677745651e86d40
青游 <2517789608@qq.com>
2022-10-02 22:30:08 +0800

    支持loading属性及样式

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,6 @@
 <template>
   <button class="gugu-button" :class="classes" :disabled="disabled">
+    <div v-if="loading" class="gugu-loadingIndicator"></div>
     <slot />
   </button>
 </template>
@@ -25,6 +26,10 @@ export default {
       type: Boolean,
       default: false,
     },
+    loading: {
+      type: Boolean,
+      default: false
+    }
   },
   setup(props) {
     const { theme, size, level } = props;
@@ -167,5 +172,20 @@ button{
       color: $grey;
     }
   }
+  > .gugu-loadingIndicator{
+    width: 14px;
+    height: 14px;
+    margin-top: 4px;
+    margin-right: 6px;
+    border-radius: 8px;
+    border-color: $blue $blue $blue transparent;
+    border-style: solid;
+    border-width: 2px;
+    animation: gugu-spin 1s infinite linear;
+  }
+}
+@keyframes gugu-spin {
+  0%{transform: rotate(0deg)}
+  100%{transform: rotate(360deg)}
 }
 </style>
