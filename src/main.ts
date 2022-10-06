2bbe0e5c3876662b93bab00030644e3ba1ece794
青游 <2517789608@qq.com>
2022-10-06 23:10:14 +0800

    引入github markdown样式，并支持引入 .md 文件

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import App from './App.vue';
 import './index.scss';
 import {router} from './router';
 import './lib/svg.js'
+import 'github-markdown-css'
 
 const app = createApp(App);
 app.mount(('#app'));
--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -1,13 +1,14 @@
 <template>
-  <article>
-    <h1>介绍</h1>
-    <p>gugu ui 是一款基于 Vue 3 和 TypeScript 的 UI 组件库。</p>
-    <p>这款组件库其实是本人作为 Vue3 练手项目而写的，全程亲手编写，尽量不采用第三方库，包括这个官网也几乎都是自己写的。</p>
-    <p><strong>不建议你将此 UI 库用于生产环境。</strong> </p>
-    <p>源代码放在了 <a src="https://github.com/qingyou2517/gugu-ui">github，</a>历史提交信息非常规范。</p>
-    <p>你可以按提交的顺序逐个查看；也可以直接查看每个组件的源代码和示例，</p>
-    <p>
-      具体运行方法见 README.md。</p>
-    下一节：<a href="#/doc/install">安装</a>
+  <article class="markdown-body" v-html="md">
+
   </article>
 </template>
+
+<script>
+import md from '../markdown/intro.md';
+export default {
+  data() {
+    return {md}
+  }
+}
+</script>
