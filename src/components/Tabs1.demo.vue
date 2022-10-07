8a591ad483595704c3eb20d0988bd53fcfc71966
青游 <2517789608@qq.com>
2022-10-07 11:18:16 +0800

    改造TabsDemo，展示Tabs子组件的源代码

---
+++ src/components/Tabs1.demo.vue
@@ -0,0 +1,27 @@
+<demo>
+常规使用
+</demo>
+
+<template>
+  <Tabs v-model:selectedTitle="title">
+    <Tab title="导航1">内容1</Tab>
+    <Tab title="导航222">内容222</Tab>
+  </Tabs>
+</template>
+
+<script lang="ts">
+import Tabs from '../lib/Tabs.vue'
+import Tab from '../lib/Tab.vue'
+import {ref} from 'vue'
+
+export default {
+  components: {
+    Tabs,
+    Tab
+  },
+  setup() {
+    const title = ref('导航222')
+    return {title}
+  }
+}
+</script>
