e0ac4cbc9c4578049fb720f63d4edf2e719acb0c
青游 <2517789608@qq.com>
2022-10-07 20:36:29 +0800

    创建 lib/index.ts，移动 svg.js

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import {createApp} from 'vue';
 import App from './App.vue';
 import './index.scss';
 import {router} from './router';
-import './lib/svg.js'
+import './assets/svg.js'
 import 'github-markdown-css'
 
 const app = createApp(App);
