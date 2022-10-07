8c690b544beef9f09bbd6debbf3ccfb6b3d8f4a0
青游 <2517789608@qq.com>
2022-10-07 20:26:05 +0800

    修复生产环境 tag.type 不等于 Tab 的问题

--- src/router.ts
+++ src/router.ts
@@ -5,7 +5,6 @@ import SwitchDemo from "./components/SwitchDemo.vue";
 import ButtonDemo from "./components/ButtonDemo.vue";
 import DialogDemo from "./components/DialogDemo.vue";
 import TabsDemo from "./components/TabsDemo.vue";
-import DocDemo from "./components/DocDemo.vue";
 
 import Intro from './views/Intro.vue';
 import GetStarted from './views/GetStarted.vue'
