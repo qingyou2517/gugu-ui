c60de09c476778ef49087b7e3033e5008fd777e5
青游 <2517789608@qq.com>
2022-10-09 00:22:29 +0800

    部署到码云

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ git init &&
 git add . &&
 git commit -m "update" &&
 git branch -M master &&
-git remote add origin git@github.com:qingyou2517/gugu-ui-website-1.git &&
+git remote add origin git@gitee.com:qilling/gugu-ui-website-1.git &&
 git push -f -u origin master &&
 cd -
 echo https://qingyou2517.github.io/gugu-ui-website-1/#/
